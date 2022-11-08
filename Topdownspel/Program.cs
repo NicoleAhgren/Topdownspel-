08eb7e6d737b085bf5d9bf39cbc1a99525a596fe
Nicole Åhgren <ahgren.nicole@elev.ga.ntig.se>
2022-11-08 08:14:09 +0100

    Hai Hai

--- Topdownspel/Program.cs
+++ Topdownspel/Program.cs
@@ -3,17 +3,27 @@
 Raylib.InitWindow(800, 600,"Najkel");
 Raylib.SetTargetFPS(60);
 
+Texture2D Gubbe = Raylib.LoadTexture("Gubbe.png");
+Texture2D RosaFjäril = Raylib.LoadTexture("RosaFjaril.png");
+
 Rectangle bla = new Rectangle (50, 60, 50, 50);
 float speed = 5f;
-Rectangle blå = new Rectangle (100, 100, 100, 100);
+Rectangle rosa = new Rectangle (600, 450, RosaFjäril.width, RosaFjäril.height);
 
-Texture2D Gubbe = Raylib.LoadTexture("Gubbe.png");
-Texture2D BlåFjäril = Raylib.LoadTexture("BlåFjäril.png");
-// bool areOverlapping = Raylib.CheckCollisionRecs(bla, blå);
+string Level = "Start";
 
 while (Raylib.WindowShouldClose() == false)
 {
+if (Level == "Start")
+{
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+    {
+        Level = "Spel";
+    }
+}
 
+else if (Level == "Spel")
+{  
     if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
     {
         bla.y -= speed;
@@ -30,14 +40,51 @@ while (Raylib.WindowShouldClose() == false)
     {
         bla.x -= speed;
     }
+}
+
+
 
     Raylib.BeginDrawing();
 
-    Raylib.ClearBackground(Color.PURPLE);
-//  Raylib.DrawRectangleRec(bla, Color.GREEN);
-    Raylib.DrawTexture(Gubbe, (int)bla.x, (int)bla.y, Color.BLACK);
+    if (Level == "Start")
+    {
+        Raylib.ClearBackground(Color.PINK);
+        Raylib.DrawText("Tryck ENTER för att spela", 150, 300, 30, Color.BLACK);
+
+    }
+
+    if (Level == "Spel")
+    { 
     
-    Raylib.DrawTexture(BlåFjäril, (int)blå.x, (int)blå.y, Color.BLACK);
+    Raylib.ClearBackground(Color.PURPLE);
+    Raylib.DrawTexture(Gubbe, (int)bla.x, (int)bla.y, Color.BLACK);
+    Raylib.DrawTexture(RosaFjäril, (int)rosa.x, (int)rosa.y, Color.PINK);
+    }
+    if (Raylib.CheckCollisionRecs(bla, rosa))
+    {
+        Level = "Start";
+    }
 
     Raylib.EndDrawing();
 }
+
+
+
+
+/*   
+if (Level == "END")
+    { 
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_HOME))
+     {
+        Level = "Start";
+     }
+    }
+
+
+        if (Level == "END")
+    {
+        Raylib.ClearBackground(Color.GREEN);
+        Raylib.DrawText("DU VANN!", 260, 250, 50, Color.BLACK);
+        Raylib.DrawText("Tryck PAUSE för att spela igen", 135, 400, 30, Color.BLACK);
+    }
+ */  
