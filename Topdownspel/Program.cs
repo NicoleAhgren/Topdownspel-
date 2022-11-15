7dc739a858c3eabb5d4a7519398163757961be35
Nicole Åhgren <ahgren.nicole@elev.ga.ntig.se>
2022-11-15 11:08:13 +0100

    kaos

--- Topdownspel/Program.cs
+++ Topdownspel/Program.cs
@@ -5,10 +5,12 @@ Raylib.SetTargetFPS(60);
 
 Texture2D Gubbe = Raylib.LoadTexture("Gubbe.png");
 Texture2D RosaFjäril = Raylib.LoadTexture("RosaFjaril.png");
+Texture2D BlåFjäril = Raylib.LoadTexture("BlaFjaril.png");
 
 Rectangle bla = new Rectangle (50, 60, 50, 50);
 float speed = 5f;
 Rectangle rosa = new Rectangle (600, 450, RosaFjäril.width, RosaFjäril.height);
+Rectangle blabla = new Rectangle (100, 100, BlåFjäril.width, BlåFjäril.height);
 
 string Level = "Start";
 
@@ -19,10 +21,11 @@ if (Level == "Start")
     if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
     {
         Level = "Spel";
+    
     }
 }
 
-else if (Level == "Spel")
+if (Level == "Spel" || Level == "Spel2")
 {  
     if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
     {
@@ -55,23 +58,41 @@ else if (Level == "Spel")
 
     if (Level == "Spel")
     { 
-    
     Raylib.ClearBackground(Color.PURPLE);
     Raylib.DrawTexture(Gubbe, (int)bla.x, (int)bla.y, Color.BLACK);
     Raylib.DrawTexture(RosaFjäril, (int)rosa.x, (int)rosa.y, Color.PINK);
     }
-    if (Raylib.CheckCollisionRecs(bla, rosa))
+    
+
+    if (Raylib.CheckCollisionRecs(bla, rosa)) //Något fel med den!!!! 
+    //När man går bort från platsen av rosa fjärilen så börjar spelet hacka och ge epelepsianfall
     {
-        Level = "Start";
+        Level = "Spel2";
+    { 
+        if (Level == "Spel2")
+        {
+    Raylib.ClearBackground(Color.PINK);
+    Raylib.DrawTexture(Gubbe, (int)bla.x, (int)bla.y, Color.BLACK);
+    Raylib.DrawTexture(BlåFjäril, (int)blabla.x, (int)blabla.y, Color.BLUE);
+
+        }
+    }
     }
 
     Raylib.EndDrawing();
+
 }
 
 
 
 
 /*   
+    if (Level2 = "Spel2")
+    {
+    Raylib.ClearBackground(Color.PINK);
+    Raylib.DrawTexture(Gubbe, (int)bla.x, (int)bla.y, Color.BLACK);
+    Raylib.DrawTexture(BlåFjäril, (int)blabla.x, (int)blabla.y, Color.BLUE);
+    }
 if (Level == "END")
     { 
     if (Raylib.IsKeyPressed(KeyboardKey.KEY_HOME))
